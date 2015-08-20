187aca88216ab86d6766bbe6863910693087de9b
PaulaNeuber <p.neuber@zeppelin-university.net>
2015-08-20 16:15:40 +0200

    22

---
+++ program4.rb
@@ -0,0 +1,7 @@
+number = 0
+(0..10).each do |n|
+  p "the new number is #{n}"
+# while (number <= 10) do # while this condition is trud, do...
+#   p "the number is now #{number}" #whatever is in this code block
+#   number += 1 #short for number=number +1
+end # dont' forget to your end
